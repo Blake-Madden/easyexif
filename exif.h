9b4118acba93b14b966f66c1ffaff3a8c9d7a655
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-08-23 05:42:21 -0400

    Initialize member variables Silences sporadic warnings from clang/GCC

--- exif.h
+++ exif.h
@@ -90,115 +90,145 @@ class EXIFInfo {
   }
 
   // Data fields filled out by parseFrom()
-  char ByteAlign;                // 0 = Motorola byte alignment, 1 = Intel
-  std::string ImageDescription;  // Image description
-  std::string Make;              // Camera manufacturer's name
-  std::string Model;             // Camera model
-  unsigned short Orientation;    // Image orientation
-                                 // 1: Horizontal (normal)
-                                 // 2: Mirror horizontal
-                                 // 3: Rotate 180
-                                 // 4: Mirror vertical
-                                 // 5: Mirror horizontal and rotate 270 CW
-                                 // 6: Rotate 90 CW
-                                 // 7: Mirror horizontal and rotate 90 CW
-                                 // 8: Rotate 270 CW
-  unsigned short BitsPerSample;  // Number of bits per component
-  double XResolution;  // Num pixels per <ResolutionUnit> - 72dpi by default
-  double YResolution;  // Num pixels per <ResolutionUnit> - 72dpi by default
-  unsigned short ResolutionUnit;  // Units for <XResolution> and <YResolution>
-                                  // 1: (unused)
-                                  // 2: inches (default)
-                                  // 3: centimetres
-  std::string Software;           // Software used
-  std::string DateTime;           // File change date and time
+  char ByteAlign{1};                // 0 = Motorola byte alignment, 1 = Intel
+  std::string ImageDescription;     // Image description
+  std::string Make;                 // Camera manufacturer's name
+  std::string Model;                // Camera model
+  unsigned short Orientation{1};    // Image orientation
+                                    // 1: Horizontal (normal)
+                                    // 2: Mirror horizontal
+                                    // 3: Rotate 180
+                                    // 4: Mirror vertical
+                                    // 5: Mirror horizontal and rotate 270 CW
+                                    // 6: Rotate 90 CW
+                                    // 7: Mirror horizontal and rotate 90 CW
+                                    // 8: Rotate 270 CW
+  unsigned short BitsPerSample{8};  // Number of bits per component
+  double XResolution{72};  // Num pixels per <ResolutionUnit> - 72dpi by default
+  double YResolution{72};  // Num pixels per <ResolutionUnit> - 72dpi by default
+  unsigned short ResolutionUnit{3};  // Units for <XResolution> and
+                                     // <YResolution> 1: (unused) 2: inches
+                                     // (default) 3: centimetres
+  std::string Software;              // Software used
+  std::string DateTime;              // File change date and time
   std::string DateTimeOriginal;   // Original file date and time (may not exist)
   std::string DateTimeDigitized;  // Digitization date and time (may not exist)
   std::string
       SubSecTimeOriginal;  // Sub-second time that original picture was taken
   std::string Copyright;   // File copyright information
-  double ExposureTime;     // Exposure time in seconds
-  double FNumber;          // F/stop
-  unsigned short ExposureProgram;  // Exposure program
-                                   // 0: Not defined
-                                   // 1: Manual
-                                   // 2: Normal program
-                                   // 3: Aperture priority
-                                   // 4: Shutter priority
-                                   // 5: Creative program
-                                   // 6: Action program
-                                   // 7: Portrait mode
-                                   // 8: Landscape mode
-  unsigned short ExposureMode;     // Exposure mode
-                                   // 0: Auto
-                                   // 1: Manual
-                                   // 2: Auto bracket
-  unsigned short WhiteBalance;     // 0 = Auto
-                                   // 1 = Manual
-  unsigned short ISOSpeedRatings;  // ISO speed
-  double ShutterSpeedValue;  // Shutter speed (reciprocal of exposure time)
-  double ExposureBiasValue;  // Exposure bias value in EV
-  double SubjectDistance;    // Distance to focus point in meters
-  double FocalLength;        // Focal length of lens in millimeters
-  unsigned short FocalLengthIn35mm;  // Focal length in 35mm film
-  unsigned short
-      FlashUnmodified;  // The raw data from the field (Not an EXIF field name)
-                        // The other three fields don't cover all possibilities
-                        // for the flash info (e.g. 0x08 "On, Did not fire")
-  char Flash;           // 0 = no flash, 1 = flash used
-  unsigned short FlashReturnedLight;  // Flash returned light status
-                                      // 0: No strobe return detection function
-                                      // 1: Reserved
-                                      // 2: Strobe return light not detected
-                                      // 3: Strobe return light detected
-  unsigned short FlashMode;           // Flash mode
-                                      // 0: Unknown
-                                      // 1: Compulsory flash firing
-                                      // 2: Compulsory flash suppression
-                                      // 3: Automatic mode
-  unsigned short MeteringMode;        // Metering mode
-                                      // 1: average
-                                      // 2: center weighted average
-                                      // 3: spot
-                                      // 4: multi-spot
-                                      // 5: multi-segment
-  unsigned short ColorSpace;          // 0x1 = sRGB
-                                      // 0x2 = Adobe RGB
-                                      // 0xfffd = Wide Gamut RGB
-                                      // 0xfffe = ICC Profile
-                                      // 0xffff = Uncalibrated
-  unsigned ImageWidth;                // Image width reported in EXIF data
-  unsigned ImageHeight;               // Image height reported in EXIF data
-  struct Geolocation_t {              // GPS information embedded in file
-    double Latitude;                  // Image latitude expressed as decimal
-    double Longitude;                 // Image longitude expressed as decimal
-    double Altitude;   // Altitude in meters, relative to sea level
-    char AltitudeRef;  // 0 = above sea level, -1 = below sea level
-    double DOP;        // GPS degree of precision (DOP)
+  double ExposureTime{
+      std::numeric_limits<double>::max()};  // Exposure time in seconds
+  double FNumber{std::numeric_limits<double>::max()};  // F/stop
+  unsigned short ExposureProgram{0};                   // Exposure program
+                                                       // 0: Not defined
+                                                       // 1: Manual
+                                                       // 2: Normal program
+                                                       // 3: Aperture priority
+                                                       // 4: Shutter priority
+                                                       // 5: Creative program
+                                                       // 6: Action program
+                                                       // 7: Portrait mode
+                                                       // 8: Landscape mode
+  unsigned short ExposureMode{
+      std::numeric_limits<unsigned short>::max()};  // Exposure mode
+                                                    // 0: Auto
+                                                    // 1: Manual
+                                                    // 2: Auto bracket
+  unsigned short WhiteBalance{
+      std::numeric_limits<unsigned short>::max()};  // 0 = Auto
+                                                    // 1 = Manual
+  unsigned short ISOSpeedRatings{
+      std::numeric_limits<unsigned short>::max()};  // ISO speed
+  double ShutterSpeedValue{
+      std::numeric_limits<double>::max()};  // Shutter speed (reciprocal of
+                                            // exposure time)
+  double ExposureBiasValue{
+      std::numeric_limits<double>::max()};  // Exposure bias value in EV
+  double SubjectDistance{
+      std::numeric_limits<double>::max()};  // Distance to focus point in meters
+  double FocalLength{
+      std::numeric_limits<double>::max()};  // Focal length of lens in
+                                            // millimeters
+  unsigned short FocalLengthIn35mm{
+      std::numeric_limits<unsigned short>::max()};  // Focal length in 35mm film
+  unsigned short FlashUnmodified{
+      std::numeric_limits<unsigned short>::
+          max()};  // The raw data from the field (Not an EXIF field name)
+                   // The other three fields don't cover all possibilities
+                   // for the flash info (e.g. 0x08 "On, Did not fire")
+  char Flash{std::numeric_limits<char>::max()};  // 0 = no flash, 1 = flash used
+  unsigned short FlashReturnedLight{std::numeric_limits<
+      unsigned short>::max()};  // Flash returned light status
+                                // 0: No strobe return detection function
+                                // 1: Reserved
+                                // 2: Strobe return light not detected
+                                // 3: Strobe return light detected
+  unsigned short FlashMode{
+      std::numeric_limits<unsigned short>::max()};  // Flash mode
+                                                    // 0: Unknown
+                                                    // 1: Compulsory flash
+                                                    // firing 2: Compulsory
+                                                    // flash suppression 3:
+                                                    // Automatic mode
+  unsigned short MeteringMode{0};                   // Metering mode
+                                                    // 1: average
+                                   // 2: center weighted average
+                                   // 3: spot
+                                   // 4: multi-spot
+                                   // 5: multi-segment
+  unsigned short ColorSpace{
+      std::numeric_limits<unsigned short>::max()};  // 0x1 = sRGB
+                                                    // 0x2 = Adobe RGB
+                                                    // 0xfffd = Wide Gamut RGB
+                                                    // 0xfffe = ICC Profile
+                                                    // 0xffff = Uncalibrated
+  unsigned ImageWidth{
+      std::numeric_limits<unsigned int>::max()};  // Image width reported in
+                                                  // EXIF data
+  unsigned ImageHeight{
+      std::numeric_limits<unsigned int>::max()};  // Image height reported in
+                                                  // EXIF data
+  struct Geolocation_t {  // GPS information embedded in file
+    double Latitude{
+        std::numeric_limits<double>::max()};  // Image latitude expressed as
+                                              // decimal
+    double Longitude{
+        std::numeric_limits<double>::max()};  // Image longitude expressed as
+                                              // decimal
+    double Altitude{
+        std::numeric_limits<double>::max()};  // Altitude in meters, relative to
+                                              // sea level
+    char AltitudeRef{0};  // 0 = above sea level, -1 = below sea level
+    double DOP{
+        std::numeric_limits<double>::max()};  // GPS degree of precision (DOP)
     struct Coord_t {
-      double degrees;
-      double minutes;
-      double seconds;
-      char direction;
+      double degrees{std::numeric_limits<double>::max()};
+      double minutes{std::numeric_limits<double>::max()};
+      double seconds{std::numeric_limits<double>::max()};
+      char direction{'?'};
     } LatComponents,
         LonComponents;  // Latitude, Longitude expressed in deg/min/sec
   } GeoLocation;
-  struct LensInfo_t {              // Lens information
-    double FStopMin;               // Min aperture (f-stop)
-    double FStopMax;               // Max aperture (f-stop)
-    double FocalLengthMin;         // Min focal length (mm)
-    double FocalLengthMax;         // Max focal length (mm)
-    double FocalPlaneXResolution;  // Focal plane X-resolution
-    double FocalPlaneYResolution;  // Focal plane Y-resolution
-    unsigned short
-        FocalPlaneResolutionUnit;  // Focal plane resolution unit
-                                   // 1: No absolute unit of measurement.
-                                   // 2: Inch.
-                                   // 3: Centimeter.
-                                   // 4: Millimeter.
-                                   // 5: Micrometer.
-    std::string Make;              // Lens manufacturer
-    std::string Model;             // Lens model
+  struct LensInfo_t {  // Lens information
+    double FStopMin{
+        std::numeric_limits<double>::max()};  // Min aperture (f-stop)
+    double FStopMax{
+        std::numeric_limits<double>::max()};  // Max aperture (f-stop)
+    double FocalLengthMin{
+        std::numeric_limits<double>::max()};  // Min focal length (mm)
+    double FocalLengthMax{
+        std::numeric_limits<double>::max()};  // Max focal length (mm)
+    double FocalPlaneXResolution{
+        std::numeric_limits<double>::max()};  // Focal plane X-resolution
+    double FocalPlaneYResolution{
+        std::numeric_limits<double>::max()};     // Focal plane Y-resolution
+    unsigned short FocalPlaneResolutionUnit{2};  // Focal plane resolution unit
+                                                 // 1: No absolute unit of
+                                                 // measurement. 2: Inch. 3:
+                                                 // Centimeter. 4: Millimeter.
+                                                 // 5: Micrometer.
+    std::string Make;                            // Lens manufacturer
+    std::string Model;                           // Lens model
   } LensInfo;
 
   EXIFInfo() { clear(); }
